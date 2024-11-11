4631b46eebc407a682829fe40eeb362297a7d2a2
ylllc <leprechaun@yggdrasil-leaves.com>
2024-11-11 22:16:55 +0000

    add trivia functions

--- api/util.js
+++ api/util.js
@@ -7,7 +7,138 @@
 
 import hap from './happening.js';
 
+const rx_zero=/^(0+(|\.)0*|0*(|\.)0+)$/;
+const rx_null=/^null$/i;
+const rx_false=/^false$/i;
+const rx_undefined=/^undefined$/i;
+
 var mif={
+
+	isJustNaN:(val)=>{
+		if(typeof val!=='number')return false;
+		return isNaN(val);
+	},
+
+	isJustInfinity:(val)=>{
+		if(val===Infinity)return true;
+		if(val===-Infinity)return true;
+		return false;
+		if(typeof val!=='number')return false;
+		return isNaN(val)===isFinite(val);
+	},
+
+	isEmpty:(val)=>{
+		if(val===null)return true;
+		if(val===undefined)return true;
+		if(val==='')return true;
+		return false;
+	},
+
+	isValid:(val)=>{
+		if(val===null)return false;
+		if(val===undefined)return false;
+		if(mif.isJustNaN(val))return false;
+		return true;
+	},
+
+	booleanize:(val)=>{
+		if(val===null)return false;
+		if(val===undefined)return false;
+		switch(typeof val){
+			case 'boolean':
+			return val;
+
+			case 'number':
+			if(isNaN(val))return true;
+			break;
+
+			case 'string':
+			if(val.match(rx_zero))return false;
+			if(val.match(rx_false))return false;
+			if(val.match(rx_null))return false;
+			if(val.match(rx_undefined))return false;
+			break;
+
+			case 'object':
+			return true;
+		}
+		return !!val;
+	},
+
+	trinarize:(val)=>{
+		if(val==null)return null;
+		if(val===undefined)return null;
+		switch(typeof val){
+			case 'string':
+			if(val.match(rx_null))return null;
+			if(val.match(rx_undefined))return null;
+			break;
+		}
+		return mif.booleanize(val);
+	},
+
+	zerofill:(val,col,sgn=false)=>{
+		var sf=val<0;
+		if(sf)val=-val;
+
+		var ss=sf?'-':sgn?'+':'';
+		col-=ss.length;
+		var vs=''+val;
+		if(vs.length>=col)return ss+vs;
+
+		vs='0'.repeat(col-1)+vs;
+		return ss+vs.substring(vs.length-col);
+	},
+
+	justString:(val)=>{
+		switch(typeof val){
+			case 'undefined': return 'undefined';
+			case 'boolean': return val?'true':'false';
+			case 'string': return val;
+			case 'number': return val.toString();
+
+			case 'object':
+			if(val===null)return 'null';
+			else if(Array.isArray(val)){
+				var sub=[]
+				for(var v of val)sub.push(mif.justString(v));
+				return '['+sub.join(',')+']';
+			}
+			else{
+				var sub=[]
+				for(var k in val)sub.push([JSON.stringify(k),mif.justString(val[k])]);
+				var sub2=[]
+				for(var s of sub)sub2.push(s.join(':'));
+				return '{'+sub2.join(',')+'}';
+			}
+			break;
+		}
+		return val.toString();
+	},
+
+	inspect:(val)=>{
+		switch(typeof val){
+			case 'undefined': return 'undefined';
+			case 'number': return val.toString();
+
+			case 'object':
+			if(val===null)return 'null';
+			else if(Array.isArray(val)){
+				var sub=[]
+				for(var v of val)sub.push(mif.inspect(v));
+				return '['+sub.join(',')+']';
+			}
+			else{
+				var sub=[]
+				for(var k in val)sub.push([JSON.stringify(k),mif.inspect(val[k])]);
+				var sub2=[]
+				for(var s of sub)sub2.push(s.join(':'));
+				return '{'+sub2.join(',')+'}';
+			}
+			break;
+		}
+		return JSON.stringify(val);
+	},
 
 	safestepiter:(bgn,end,step,cbiter)=>{
 
---
+++ example/002-trivia.js
@@ -0,0 +1,126 @@
+// † Yggdrasil Essense for JavaScript † //
+// ====================================== //
+// © 2024 Yggdrasil Leaves, LLC.          //
+//        All rights reserved.            //
+
+// Examples: Value Trivia //
+
+import log from '../api/logger.js';
+import util from '../api/util.js';
+
+function test(func,data){
+
+	for(var f in func){
+		log.info('-=-=-=-=-=-=-=-=- '+f+' -=-=-=-=-=-=-=-=-');
+		for(var d in data){
+			try{log.info(d+' => '+func[f](data[d]));}
+			catch(e){log.fatal(e.message);}
+		}
+	}
+}
+
+test({
+	'==NaN':(val)=>(val==NaN)?'true':'false',
+	'===NaN':(val)=>(val===NaN)?'true':'false',
+	'isNaN':(val)=>isNaN(val)?'true':'false',
+	'isJustNaN':(val)=>util.isJustNaN(val)?'true':'false',
+},{
+	'NaN':NaN,
+	'undefined':undefined,
+	'"A"':"A",
+	'{}':{},
+	'[]':[],
+	'[0]':[0],
+	'false':false,
+	'[false]':[false],
+});
+
+test({
+	'==Infinity':(val)=>(val==Infinity)?'true':'false',
+	'===Infinity':(val)=>(val===Infinity)?'true':'false',
+	'!isFinite':(val)=>!isFinite(val)?'true':'false',
+	'isJustInfinity':(val)=>util.isJustInfinity(val)?'true':'false',
+},{
+	'Infinity':Infinity,
+	'-Infinity':-Infinity,
+	'"Infinity"':"Infinity",
+	'NaN':NaN,
+	'undefined':undefined,
+	'false':false,
+	'[false]':[false],
+});
+
+test({
+	'!':(val)=>!val?'true':'false',
+	'==null':(val)=>(val==null)?'true':'false',
+	'===null':(val)=>(val===null)?'true':'false',
+	'==undefined':(val)=>(val==undefined)?'true':'false',
+	'===undefined':(val)=>(val===undefined)?'true':'false',
+	'isEmpty':(val)=>util.isEmpty(val)?'true':'false',
+},{
+	'""':"",
+	'0':0,
+	'false':false,
+	'null':null,
+	'undefined':undefined,
+	'[]':[],
+	'{}':{},
+	'NaN':NaN,
+});
+
+test({
+	'!!':(val)=>!!val?'true':'false',
+	'isValid':(val)=>util.isValid(val)?'true':'false',
+	'booleanize':(val)=>util.booleanize(val),
+	'trinarize':(val)=>util.trinarize(val),
+},{
+	'""':"",
+	'0':0,
+	'1':1,
+	'".00"':".00",
+	'false':false,
+	'"false"':"false",
+	'"FaLsE"':"FaLsE",
+	'null':null,
+	'"null"':"null",
+	'undefined':undefined,
+	'"undefined"':"undefined",
+	'[]':[],
+	'{}':{},
+	'NaN':NaN,
+	'"NaN"':"NaN",
+});
+
+test({
+	'padStart':(val)=>(''+val).padStart(8,'0'),
+	'zerofill':(val)=>util.zerofill(val,8),
+	'zerofill+':(val)=>util.zerofill(val,8,true),
+},{
+	'0':0,
+	'123.4':123.4,
+	'-12.3':-12.3,
+	'1234.5678':1234.5678,
+});
+
+test({
+	'toString':(val)=>val.toString(),
+	'justString':(val)=>util.justString(val),
+	'JSON':(val)=>JSON.stringify(val),
+	'inspect':(val)=>util.inspect(val),
+},
+{
+	'false':false,
+	'true':true,
+	'null':null,
+	'undefined':undefined,
+	'-Infinity':-Infinity,
+	'NaN':NaN,
+	'0':0,
+	'"0"':"0",
+	'[]':[],
+	'[null]':[null],
+	'[undefined]':[undefined],
+	'{}':{},
+	'{a:-1,b:"xyz"}':{a:-1,b:"xyz"},
+});
+
