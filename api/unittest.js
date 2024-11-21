f5f13956bc8765ba83b5bcf5ef86deed5dbb9551
ylllc <leprechaun@yggdrasil-leaves.com>
2024-11-21 12:18:32 +0000

    additional test funcs

--- api/unittest.js
+++ api/unittest.js
@@ -8,13 +8,25 @@
 import assert from 'node:assert';
 import test from 'node:test';
 
+import util from './util.js';
+
+function _cpmsg(msg,v1,op,v2){
+	if(!msg)msg='Test Mismatch:';
+	return ''+msg+' ('+util.inspect(v1)+' '+op+' '+util.inspect(v2)+')';
+}
+
 export default {
 	name:'YgEs_UnitTest',
 	User:{},
 
-	chk:(cond,msg)=>{assert(cond,msg)},
-	chk_loose:(v1,v2,msg)=>{assert(v1==v2,msg)},
-	chk_strict:(v1,v2,msg)=>{assert(v1===v2,msg)},
+	never:(msg=null)=>{assert(false,msg)},
+	chk:(cond,msg=null)=>{assert(cond,msg)},
+	chk_loose:(v1,v2,msg=null)=>{assert(v1==v2,_cpmsg(msg,v1,'==',v2))},
+	chk_strict:(v1,v2,msg=null)=>{assert(v1===v2,_cpmsg(msg,v1,'===',v2))},
+	chk_less:(v1,v2,msg=null)=>{assert(v1<v2,_cpmsg(msg,v1,'<',v2))},
+	chk_less_eq:(v1,v2,msg=null)=>{assert(v1<=v2,_cpmsg(msg,v1,'<=',v2))},
+	chk_great:(v1,v2,msg=null)=>{assert(v1>v2,_cpmsg(msg,v1,'>',v2))},
+	chk_great_eq:(v1,v2,msg=null)=>{assert(v1>=v2,_cpmsg(msg,v1,'>=',v2))},
 
 	run:(scn)=>{
 
