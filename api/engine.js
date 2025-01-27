90fddc4dba654e8d27081499522dca0710673917
ylllc <leprechaun@yggdrasil-leaves.com>
2025-01-27 08:46:34 +0000

    add Procedure.GetInfo()

--- api/engine.js
+++ api/engine.js
@@ -43,6 +43,8 @@ function _create_proc(prm){
 		IsAborted:()=>aborted,
 		IsEnd:()=>finished||aborted,
 
+		GetInfo:()=>{return {}},
+
 		_start:()=>{
 			if(started)return;
 			if(proc.IsEnd())return;
