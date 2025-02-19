a394c73ff5c7053802cd30c947bc2c571e00e603
ylllc <leprechaun@yggdrasil-leaves.com>
2025-02-19 14:11:53 +0000

    add transport simulation

---
+++ example/060-transport.js
@@ -0,0 +1,87 @@
+// † Yggdrasil Essense for JavaScript † //
+// ====================================== //
+// © 2025 Yggdrasil Leaves, LLC.          //
+//        All rights reserved.            //
+
+// Example: Transport Simulation -------- //
+
+import Transport from '../api/transport.js';
+import EndPoint from '../api/endpoint.js';
+import Timing from '../api/timing.js';
+import Engine from '../api/engine.js';
+import HappeningManager from '../api/happening.js';
+import Log from '../api/logger.js';
+
+// start the Engine 
+Engine.Start();
+
+// for environment 
+let log_local=Log.CreateLocal('TransportTest');
+let launcher=Engine.CreateLauncher();
+let hap_local=HappeningManager.CreateLocal({
+	OnHappen:(hap)=>{log_local.Fatal(hap.GetProp());},
+});
+
+// transport setting
+const topt={
+	Log:log_local,
+	Launcher:launcher,
+	HappenTo:hap_local,
+	// (for tough test) insert random msec delay 
+	DelayMin:0,
+	DelayMax:400,
+	// (for tough test) maybe break ordering by delay 
+	Unorderable:false,
+	// (for tough test) ratio of short packet on sending 
+	Hurting:0.0,
+}
+
+// endpoint setting
+const eopt={
+	Log:log_local,
+	Launcher:launcher,
+	HappenTo:hap_local,
+	OnReceived:(ep,from,data)=>{
+		log_local.Info('EndPoint '+ep.GetInstanceID()+' received from '+from,data);
+	},
+}
+
+// loopback endpoints 
+let lb_tp=Transport.CreateLoopback(topt).Open();
+let lb_ep1=EndPoint.Create(lb_tp.GetAgent(),eopt).Open();
+let lb_ep2=EndPoint.Create(lb_tp.GetAgent(),eopt).Open();
+// send each other 
+// (when onorderable delay test, maybe receive B to A) 
+lb_ep1.Send(lb_ep2.GetInstanceID(),'Loopback Test1A');
+lb_ep2.Send(lb_ep1.GetInstanceID(),'Loopback Test2A');
+lb_ep1.Send(lb_ep2.GetInstanceID(),'Loopback Test1B');
+lb_ep2.Send(lb_ep1.GetInstanceID(),'Loopback Test2B');
+// send to me 
+// always received in random order from another senders 
+lb_ep1.Send(lb_ep1.GetInstanceID(),'Loopback Myself Test1');
+lb_ep2.Send(lb_ep2.GetInstanceID(),'Loopback Myself Test2');
+
+// terminate endpoints 
+let tm_tp=Transport.CreateTerminator(topt).Open();
+let tm_ep1=EndPoint.Create(tm_tp.GetAgent(),eopt).Open();
+let tm_ep2=EndPoint.Create(tm_tp.GetAgent(),eopt).Open();
+// sendings will ignored 
+tm_ep1.Send(tm_ep2.GetInstanceID(),'Terminate Test1');
+tm_ep2.Send(tm_ep1.GetInstanceID(),'Terminate Test2');
+
+(async ()=>{
+
+	await Timing.DelayKit(1000).ToPromise();
+
+	lb_ep1.Close();
+	lb_ep2.Close();
+	tm_ep1.Close();
+	tm_ep2.Close();
+
+	lb_tp.Close();
+	tm_tp.Close();
+
+	launcher.Sync((dmy)=>{
+		Engine.ShutDown();
+	});
+})();
